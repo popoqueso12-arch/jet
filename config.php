169b82854ee2c74f298f4b2a00b8d9623be683c2
popoqueso12-arch <popoqueso12@gmail.com>
2026-09-17 14:11:46 -0500

    Fix: actualizar URL permanente de B4A

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 return [
     "bot_token" => "8714922704:AAG9dcP56xY_gdUktBusuZFMdlj5Aqo2p4k",
     "chat_id" => "-5234970591",
-    "webhook_url" => "https://jets-z2fophe7.b4a.run/webhook.php"
+    "webhook_url" => "https://jets-8jev79ij.b4a.run/webhook.php"
 ];
